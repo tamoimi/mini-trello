9b165ef97c83a2088d3f2d76c61c98eaae2fdd52
tami <tamiortami@gmail.com>
2024-12-26 10:13:32 +0900

    feat: DragabbleCard component

---
+++ src/Components/DragabbleCard.tsx
@@ -0,0 +1,28 @@
+import { Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const Card = styled.div`
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px 10px;
+  background-color: ${(props) => props.theme.cardColor};
+`;
+
+interface IDranabbleCardProps {
+  toDo: string;
+  index: number;
+}
+
+const DragabbleCard = ({ toDo, index }: IDranabbleCardProps) => {
+  return (
+    <Draggable key={toDo} draggableId={toDo} index={index}>
+      {(magic) => (
+        <Card ref={magic.innerRef} {...magic.dragHandleProps} {...magic.draggableProps}>
+          {toDo}
+        </Card>
+      )}
+    </Draggable>
+  );
+};
+
+export default DragabbleCard;

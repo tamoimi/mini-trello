0c1be9425d61b9960fd49c4948330618eda6387e
tami <tamiortami@gmail.com>
2025-01-10 16:21:07 +0900

    feat: new states

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,10 +1,14 @@
 import { atom } from "recoil";
 
-export const toDoState = atom({
+interface IToDoState {
+  [key: string]: string[];
+}
+
+export const toDoState = atom<IToDoState>({
   key: "toDo",
   default: {
-    to_do: ["a", "b", "c", "d", "e", "f"],
-    doing: [],
-    done: [],
+    "To Do": ["a", "b"],
+    Doing: ["c", "d", "e"],
+    Done: ["f"],
   },
 });
